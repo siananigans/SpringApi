6011c7cc1996f2aa2fa0572ba36f2a54e85cfb8b
Sian <sianlennon109@gmail.com>
2024-02-13 22:58:12 +0000

    First Commit

--- src/main/java/org/example/api/controller/UserController.java
+++ src/main/java/org/example/api/controller/UserController.java
@@ -1,4 +1,27 @@
 package org.example.api.controller;
 
+import org.example.api.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        Optional<User> user = userService.getUser(id);
+        return user.orElse(null);
+    }
 }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,4 +1,40 @@
 package org.example.service;
 
+import org.example.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class UserService {
+
+    private List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+
+        User user1 = new User(1,"Sian", 32, "sian@mail.com");
+        User user2 = new User(2,"Aaron", 26, "aaron@mail.com");
+        User user3 = new User(3,"Lars", 45, "lars@mail.com");
+        User user4 = new User(4,"Ben", 32, "ben@mail.com");
+        User user5 = new User(5,"Eva", 59, "eva@mail.com");
+
+        userList.addAll(Arrays.asList(user1,user2,user3,user4,user5));
+
+    }
+
+    public Optional<User> getUser(Integer id) {
+        Optional optional = Optional.empty();
+        for (User user: userList) {
+            if(id == user.getId()) {
+                optional = Optional.of(user);
+                return optional;
+            }
+        }
+        return optional;
+    }
 }
